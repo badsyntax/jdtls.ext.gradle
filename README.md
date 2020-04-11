3959df6456f05672e94d9562ef1ba3c3656711b9
Richard Willis <willis.rh@gmail.com>
2020-04-11 21:02:51 +0100

    Add initital files

---
+++ README.md
@@ -0,0 +1,7 @@
+# eclipse.jdt.ls extension (Gradle)
+
+**UNTESTED AND PROBABLY DOESN'T WORK YET**
+
+This project demonstrates how to build an eclipse.jdt.ls (Eclipse Java Development Kit Language Server) extension using Gradle instead of Maven.
+
+This project extends the JDT.LS by providing an extension point for `org.eclipse.jdt.ls.core.delegateCommandHandler`. See `SampleDelegateCommandHandler`.
---
+++ gradletasks.core/src/main/java/com/github/badsyntax/gradletasks/internal/SampleDelegateCommandHandler.java
@@ -0,0 +1,21 @@
+package com.github.badsyntax.gradletasks.internal;
+
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.ls.core.internal.IDelegateCommandHandler;
+
+public class SampleDelegateCommandHandler implements IDelegateCommandHandler {
+
+    public static final String COMMAND_ID = "gradletasks.samplecommand";
+
+    @Override
+    public Object executeCommand(String commandId, List<Object> arguments, IProgressMonitor progress) throws Exception {
+        if (COMMAND_ID.equals(commandId)) {
+            return "Hello World";
+        }
+        throw new UnsupportedOperationException(String.format("Unsupported command '%s'!", commandId));
+    }
+
+
+}
